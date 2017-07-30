bc1ea7a422f447f65be9bbea2149a369675b27a8
xiansheng123 <732824600@qq.com>
2017-07-30 21:19:13 +0800

    Remove random comments

--- src/test/java/com/example/demo_web/Util/GenericPersonTest.java
+++ src/test/java/com/example/demo_web/Util/GenericPersonTest.java
@@ -4,6 +4,7 @@ import com.example.demo_web.Entity.Person;
 import org.junit.Test;
 
 import java.time.Period;
+import java.util.Hashtable;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +14,7 @@ import static org.junit.Assert.*;
 public class GenericPersonTest {
 
     @Test
-  public   void genericPerson() {
+    public void genericPerson() {
         GenericPerson<Person> myperson = new GenericPerson<>();
         myperson.setPersonDB(Person.builder()
                 .firstname("xuda")
@@ -21,5 +22,21 @@ public class GenericPersonTest {
                 .age(28).build()
         );
         System.out.println(myperson.toString());
+    }
+
+    @Test
+    public void HashTableTest() {
+        Hashtable<String, Person> mytable = new Hashtable<String, Person>() {{
+            put("First", Person.builder()
+                    .firstname("xuda")
+                    .lastname("lu")
+                    .age(28).build());
+            put("Second", Person.builder()
+                    .firstname("xuda2")
+                    .lastname("lu2")
+                    .age(29).build());
+        }};
+        System.out.println(mytable.get("First"));
+        System.out.println(mytable.get("Second"));
     }
 }
